2d73f3feff34b5e391e55bf14c30f6d156145162
Medya Gh <medya.gh@gmail.com>
2013-09-26 16:02:59 -0500

    Adding Point interface, with two classes (Cartesian and Polar )that implements tem

--- JavaDummyTests/src/DummyPackage/Main.java
+++ JavaDummyTests/src/DummyPackage/Main.java
@@ -1,10 +1,15 @@
 package DummyPackage;
-
+import java.awt.Color;
 final public class Main {
 	
 	public static void main(String args[]) {
 		
 		System.out.println("Hello World !");
+		CartesianPoint p1= new CartesianPoint (3,4,Color.RED);
+		p1.getColor();
+		Point p2= p1; 
+		p2.getX();
+		
 	}
 	
 } 
